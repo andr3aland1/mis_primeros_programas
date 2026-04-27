1bc539978f97af9cdfc436bf26f72513f1d7dec1
andr3aland1 <andrealandi@hotmail.es>
2026-04-27 10:25:07 -0300

    ...

---
+++ VI Estructuras_repetidas_incondicionales/act_8_precipitaciones.py
@@ -0,0 +1,16 @@
+""" Un cliente ha solicitado un programa que le permita ingresar los mililitros de lluvia caídos diariamente en una semana, 
+para que el programa le informe en pantalla el promedio de precipitación de esa semana. El cliente también desea saber 
+cuál fue el día en que más llovió en la semana. 
+A modo ilustrativo, un reporte generado por el programa debería verse, luego de haber leído las precipitaciones de los 
+7 días de la semana, así: 
+            El promedio de precipitaciones fue de XX ml diarios. 
+            El día de más precipitaciones fue el xxxxxx (nombre del día). 
+Ten en cuenta que la numeración de los días de la semana comienza con el 1 para el día domingo. 
+Codifica el programa para dar solución a lo solicitado por el cliente. """
+
+
+precipitaciones = int(input("Ingrese la cantidad en mililitros de precipitaciones: ")
+
+for i in range (1,8)
+    if i == 1
+    
