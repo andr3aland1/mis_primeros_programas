dc3edfa8fd1d99ef46fef0350e8d3a24a113c1be
andr3aland1 <andrealandi@hotmail.es>
2026-04-27 21:45:16 -0300

    ...

---
+++ VI Estructuras_repetidas_incondicionales/act_cuadras.py
@@ -0,0 +1,12 @@
+""" Un cliente ha solicitado un programa que le permita ingresar cuántas cuadras caminó con su perro 
+durante una cantidad de días determinada por él mismo.
+El programa debe retornar al finalizar:
+La cantidad total de cuadras caminadas.
+El promedio de cuadras por día.
+El número de día (empezando desde 1) en el que más caminó.
+Además:
+Si alguno de los días caminó más de 30 cuadras, mostrar el mensaje:
+ "El perro necesita 24 horas de descanso"
+Si el promedio es menor a 10 y ningún día caminó más de 20 cuadras, mostrar:
+ "El perro necesita caminar más"’ """
+
