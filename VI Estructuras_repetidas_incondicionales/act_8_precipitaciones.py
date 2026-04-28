ccb34b79b408ec8e2c9c183f6c0f17058490050b
andr3aland1 <andrealandi@hotmail.es>
2026-04-27 21:10:06 -0300

    ...

--- VI Estructuras_repetidas_incondicionales/act_8_precipitaciones.py
+++ VI Estructuras_repetidas_incondicionales/act_8_precipitaciones.py
@@ -8,17 +8,29 @@ A modo ilustrativo, un reporte generado por el programa debería verse, luego de
 Ten en cuenta que la numeración de los días de la semana comienza con el 1 para el día domingo. 
 Codifica el programa para dar solución a lo solicitado por el cliente. """
 
-precipitaciones = 0
-
-for i in range (1,8)
+acumulador = 0
+for i in range (1,8):
     precipitaciones = int(input("Ingrese la cantidad en mililitros de precipitaciones: "))
-    acumulador = precipitaciones + precipitaciones
-    if i == 1
-    maximo = numero
-    posicion = i
-    if maximo < numero
-    maximo = numero
-    posicion = i
-elif posicion == i
+    acumulador = acumulador + precipitaciones
+    if i == 1:
+        maximo = precipitaciones
+        posicion = i
+    elif maximo < precipitaciones:
+        maximo = precipitaciones
+        posicion = i
 promedio = acumulador / 7
-print (f"El promedio de lluvias fue ")
+print (f"El promedio de precipitaciones fue de {promedio:.2f} ml diarios")
+if posicion == 1:
+    print("El día con más precipitaciones fue el día domingo")
+elif posicion == 2:
+    print("El día con más precipitaciones fue el día lunes")
+elif posicion == 3:
+    print("El día con más precipitaciones fue el día martes")
+elif posicion == 4:
+    print("El día con más precipitaciones fue el día miércoles")
+elif posicion == 5:
+    print("El día con más precipitaciones fue el día jueves")
+elif posicion == 6:
+    print("El día con más precipitaciones fue el día viernes")
+else:
+    print("El día con más precipitaciones fue el día sábado")
