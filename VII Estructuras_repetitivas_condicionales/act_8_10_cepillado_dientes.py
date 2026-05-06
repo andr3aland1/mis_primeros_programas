d91d5d24d1bce5291e59fb8bd44c54def3784b40
andr3aland1 <andrealandi@hotmail.es>
2026-05-06 00:04:24 -0300

    ...

---
+++ VII Estructuras_repetitivas_condicionales/act_8_10_cepillado_dientes.py
@@ -0,0 +1,40 @@
+""" Si bien el while es útil cuando desconocemos la cantidad de veces que repetiremos un bloque de instrucciones,
+también puede ser utilizado en los mismos casos que es utilizado el For (aunque la inversa no es verdadera). 
+Rehaga todos los ejercicios del Trabajo Práctico VI utilizando un While en lugar de un For. """
+
+""" Un odontólogo pide un programa donde el paciente ingrese cuantas veces se cepilló los dientes cada día 
+durante una semana (7 días).
+El programa debe :  
+• Contar cuantos días se cepillo 3 o más veces.
+• Si ningún día se cepilló más de una vez mostrar por pantalla “Higiene insuficiente”. 
+• Mostrar (si hubo) cuántos días no se cepillo los dientes. 
+• Debe retornar la cantidad de días que se cepillo 3 veces o más y promedio de cepilladas diarias. """
+
+mas_tres = 0
+no_cepillo = 0
+insuficiente = True
+cepilladas_total = 0
+contador = 0
+
+while contador != 7:
+    contador += 1
+    cepillado = int(input(f"Ingrese cuantas veces se cepilló los dientes el {contador}º día de la semana: "))
+    cepilladas_total += cepillado
+    
+    if cepillado == 0:
+        no_cepillo += 1
+    elif cepillado > 1:
+        insuficiente = False
+
+    if cepillado >= 3:
+        mas_tres += 1
+    
+promedio = cepilladas_total / 7
+
+if insuficiente:
+    print("Higene insuficiente")
+
+if no_cepillo > 0:
+    print(f"Usted no se cepilló los dientes {no_cepillo} días")
+
+print(f"Usted se cepilló 3 veces o más: {mas_tres} días.\nSe cepilló por día un promedio de {promedio:.2f}")
---
+++ VII Estructuras_repetitivas_condicionales/act_8_9_cuadras.py
@@ -0,0 +1,43 @@
+""" Si bien el while es útil cuando desconocemos la cantidad de veces que repetiremos un bloque de instrucciones,
+también puede ser utilizado en los mismos casos que es utilizado el For (aunque la inversa no es verdadera). 
+Rehaga todos los ejercicios del Trabajo Práctico VI utilizando un While en lugar de un For. """
+
+""" Un cliente ha solicitado un programa que le permita ingresar cuántas cuadras caminó con su perro 
+durante una cantidad de días determinada por él mismo.
+El programa debe retornar al finalizar:
+La cantidad total de cuadras caminadas.
+El promedio de cuadras por día.
+El número de día (empezando desde 1) en el que más caminó.
+Además:
+Si alguno de los días caminó más de 30 cuadras, mostrar el mensaje:
+ "El perro necesita 24 horas de descanso"
+Si el promedio es menor a 10 y ningún día caminó más de 20 cuadras, mostrar:
+ "El perro necesita caminar más" """
+
+total_cuadras = 0
+contador = 0
+
+dias = int(input("Ingrese la cantidad de días que caminó con su perro: "))
+
+while dias != contador:
+    contador += 1
+    cuadras = int(input(f"Ingrese la cantidad de cuadras que caminó el día {contador}: "))
+    total_cuadras = total_cuadras + cuadras
+
+    if contador == 1:
+        maximo_cuadras = cuadras
+        posicion_max = contador 
+    
+    elif maximo_cuadras < cuadras:
+        maximo_cuadras = cuadras
+        posicion_max = contador 
+
+promedio = total_cuadras / dias
+
+print(f"El perro caminó {total_cuadras} cuadras con un promedio de {promedio:.2f} cuadras caminadas por día.")
+print(f"El {posicion_max}º día fue el día que más camino.")
+
+if promedio < 10 and maximo_cuadras < 20:
+    print("El perro necesita caminar más")
+elif maximo_cuadras > 30:
+    print("El perro necesita 24 horas de descanso")
