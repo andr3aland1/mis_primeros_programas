e25b5fd24af4aa68d4be7b93785322a428f22523
andr3aland1 <andrealandi@hotmail.es>
2026-05-06 21:59:57 -0300

    ...

--- VIII Validación_de_datos/act_3_2_edad.py
+++ VIII Validación_de_datos/act_3_2_edad.py
@@ -7,12 +7,12 @@ Verifica que el dato ingresado sea válido, teniendo en cuenta que la edad es un
 y el rango válido para este programa es de 18 a 60 años. 
 El programa debe solicitar el reingreso de manera indefinida, hasta que el dato sea correcto """
 
-edad = int(input("Ingrese su edad: "))
+edad = input("Ingrese su edad: ")
 intentos = 0
 
-while (edad < 18 or edad > 60) and intentos < 10:
+while (not edad.isdigit() or int(edad) < 18 or int(edad) > 60) and intentos < 10:
     intentos += 1
-    edad = int(input("El dato no es válido.Ingrese su edad: "))
+    edad = input("El dato no es válido.Ingrese su edad: ")
 
 if intentos == 10:
 
